9a2b18c5cf6055d1d86d27ba31bf0b853372232c
benos <benantonn@gmail.com>
2023-04-12 13:10:23 -0500

    add fetch+fill functionality
    Added functionality to search for a city and fetch weather data from API
    convert to correct format and fill in html fields. Some slight style changes.

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
-// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
-
-
-
 $(document).ready(function() {
     $("#cityButton").click(function (event) {
         var formContent = $("#city").val();
-        alert(formContent);
-
         fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + formContent + '&limit=1&appid=a3ca7c6257bb748d4cf6def79e5d06d0')
             .then(response => response.json())
             .then(data => {
@@ -17,6 +11,12 @@ $(document).ready(function() {
                 .then(response => response.json())
                 .then(data => {
                         console.log(data);
+                        console.log(data.list[0].main.feels_like);
+                        for (let i = 0; i < 7; i++) {
+                            $('#day' + i + "weather").find('.temp').text("Temp: " + ((data.list[i].main.feels_like - 273.15) * (9/5) + 32).toFixed(2) + " F°");
+                            $('#day' + i + "weather").find('.wind').text("Wind: " + (data.list[i].wind.speed).toFixed(2) + " MPH");
+                            $('#day' + i + "weather").find('.humid').text("Humidity: " + (data.list[i].main.humidity) + "%");
+                        }
                 })
             })
     });
