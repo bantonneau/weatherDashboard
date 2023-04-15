d112b5c234bc2ecd668ab22eed15c149f2d64618
benos <benantonn@gmail.com>
2023-04-14 19:55:11 -0500

    add comments

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
+    // Get current date using Day.js library
     var todaysDate = dayjs();
+    // Retrieve stored values from local storage
     retrieveFromLocalStorage();
-    
+
 
     // Function to save an array to local storage
     function saveToLocalStorage(value) {
@@ -15,7 +17,7 @@ $(document).ready(function () {
         if (myArray.length > 5) {
             myArray.pop();
         }
-
+        // Store the array in local storage
         localStorage.setItem(localStorageKey, JSON.stringify(myArray));
     }
 
@@ -27,6 +29,7 @@ $(document).ready(function () {
         const buttonContainer = document.getElementById("button-container");
         buttonContainer.innerHTML = "";
 
+        // Create a button element for each stored value and append it to the button container
         myArray.forEach((value) => {
             const button = document.createElement("button");
             button.innerText = value;
@@ -35,24 +38,30 @@ $(document).ready(function () {
         });
     }
 
-
+    // Click event for the "search" button
     $("#cityButton").click(function (event) {
+        // Show weather icon
         $('.wIcon').css('display', 'block');
+        // Get the value of the input field and store it in local storage
         var formContent = $("#city").val();
         saveToLocalStorage(formContent);
         retrieveFromLocalStorage();
+        // Update the city display text
         $("#cityDisplay").text(formContent);
+        // Fetch the latitude and longitude for the entered city using the OpenWeatherMap API
         fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + formContent + '&limit=1&appid=a3ca7c6257bb748d4cf6def79e5d06d0')
             .then(response => response.json())
             .then(data => {
                 console.log(data);
                 console.log(data[0].lat);
                 console.log(data["lon"]);
+                // Fetch the weather forecast for the location using the OpenWeatherMap API
                 fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + data[0].lat + '&lon=' + data[0].lon + '&appid=a3ca7c6257bb748d4cf6def79e5d06d0')
                     .then(response => response.json())
                     .then(data => {
                         console.log(data);
                         console.log(data.list[0].main.feels_like);
+                        // Update the weather display for the next 7 days
                         for (let i = 0; i < 7; i++) {
                             $('#day' + i + "weather").find('.date').text(todaysDate.add(i, "day").format('MM/DD/YYYY'));
                             $('#day' + i + "weather").find('.wIcon').attr('src', "http://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png");
@@ -65,20 +74,25 @@ $(document).ready(function () {
     });
 
     $(document).on("click", ".historyButton", function (event) {
+        // Displays the weather icons.
         $('.wIcon').css('display', 'block');
+        // Gets the text content of the clicked button and displays it in the city display element.
         var formContent = $(this).text();
         $("#cityDisplay").text(formContent);
+        // Fetches the latitude and longitude coordinates of the city using the OpenWeatherMap API.
         fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + formContent + '&limit=1&appid=a3ca7c6257bb748d4cf6def79e5d06d0')
             .then(response => response.json())
             .then(data => {
                 console.log(data);
                 console.log(data[0].lat);
                 console.log(data["lon"]);
+                // Fetches the weather forecast for the city using the OpenWeatherMap API.
                 fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + data[0].lat + '&lon=' + data[0].lon + '&appid=a3ca7c6257bb748d4cf6def79e5d06d0')
                     .then(response => response.json())
                     .then(data => {
                         console.log(data);
                         console.log(data.list[0].main.feels_like);
+                        // Updates the HTML for each of the next 5 days in the forecast with the relevant weather information.
                         for (let i = 0; i < 7; i++) {
                             $('#day' + i + "weather").find('.date').text(todaysDate.add(i, "day").format('MM/DD/YYYY'));
                             $('#day' + i + "weather").find('.wIcon').attr('src', "http://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png");
